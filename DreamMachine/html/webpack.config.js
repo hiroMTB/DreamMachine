e0cf890c2d43ceaff9fbe311309f8234578588bb
Hiroshi Matoba <matobahiroshi@gmail.com>
2023-02-16 13:23:55 +0100

    webpack

--- DreamMachine/html/webpack.config.js
+++ DreamMachine/html/webpack.config.js
@@ -10,6 +10,18 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.svg/,
+                type: 'asset/resource',
+            },
+        ],
+    },
     // optimization: {
     //     runtimeChunk: 'single',
     // },
