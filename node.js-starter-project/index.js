b81d266cdd20f24b295c9ccb2da5e85694d6418d
Hiroshi Matoba <matobahiroshi@gmail.com>
2023-03-07 23:14:31 +0100

    add starter project from BrightSign official github repo

---
+++ node.js-starter-project/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+function main() {
+  
+  app.use(express.static('/storage/sd'));
+
+  app.listen(9090, function() {
+    console.log('Example app listening on port 9090!');
+  });
+}
+
+// exchange below line to 'window.main = main;' if using node-server.html
+// as the application entry point.
+main();
+// window.main = main;
